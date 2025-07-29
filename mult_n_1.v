683f5292106aa91c39612346d22d76b6fc3d4851
Brankica-13 <mejlzaucenje1@gmail.com>
2025-07-29 20:20:18 +0200

    Update mult_n_1.v

--- mult_n_1.v
+++ mult_n_1.v
@@ -2,6 +2,6 @@ Theorem mult_n_1 : forall p : nat,
   p * 1 = p.
 Proof.
   intros.
-  rewrite <- mult_n_Sm.   (** This writes p*1 as p*0 + p *)
+rewrite <- mult_n_Sm.   (** This writes p*1 as p*0 + p because definition of mulltiplication in recursive and here we multipled p with number n ( in this case 0) an added another p to get multiplication with succesor of n *)
   rewrite <- mult_n_O.    (** This writes p*0 as 0 *)
   reflexivity. Qed.
