1043cdc5eb64d810b681c1c3c5303e30631bcb9f
Brankica-13 <mejlzaucenje1@gmail.com>
2025-07-31 16:52:03 +0200

    Create andb_eq_orb.v

---
+++ andb_eq_orb.v
@@ -0,0 +1,16 @@
+Theorem andb_eq_orb :
+  forall (b c : bool),
+  (andb b c = orb b c) ->
+  b = c.
+Proof.
+intros b c.
+destruct b.
+- simpl.
+intro H.
+rewrite H.
+reflexivity.
+-simpl.
+intro H.
+rewrite H.
+reflexivity.
+
