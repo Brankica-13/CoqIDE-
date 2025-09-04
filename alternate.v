898da4fc80eb01c08a0b19a856789297c6c83eab
Brankica-13 <mejlzaucenje1@gmail.com>
2025-09-04 20:01:15 +0200

    Create alternate.v

---
+++ alternate.v
@@ -0,0 +1,41 @@
+Inductive natlist : Type :=
+  | nil
+  | cons (n : nat) (l : natlist).
+
+Notation "x :: l" := (cons x l)
+                     (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+    
+Fixpoint alternate (l1 l2 : natlist) : natlist :=
+match l1, l2 with
+  | h1 :: l1', h2 :: l2' => h1 :: h2 :: alternate l1' l2'
+  | l1 , nil => l1
+  | nil , l2 => l2
+end.
+ 
+
+Example test_alternate1:
+  alternate [1;2;3] [4;5;6] = [1;4;2;5;3;6].
+Proof. reflexivity. Qed.
+
+Example test_alternate2:
+  alternate [1] [4;5;6] = [1;4;5;6].
+Proof. reflexivity. Qed.
+
+Example test_alternate3:
+  alternate [1;2;3] [4] = [1;4;2;3].
+Proof. reflexivity. Qed.
+
+Example test_alternate4:
+  alternate [] [20;30] = [20;30].
+Proof. reflexivity. Qed.
+
+
+
+
+
+
+
+
+
